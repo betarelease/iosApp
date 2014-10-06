ac6f77023324eddf8efb799875a17dd853e9773c
Sudhindra Rao <sudhindra.r.rao@gmail.com>
2014-10-06 11:52:51 -0700

    Initial Commit

---
+++ helloworld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  helloworld
+//
+//  Created by Sudhindra Rao on 10/6/14.
+//  Copyright (c) 2014 Sudhindra Rao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
